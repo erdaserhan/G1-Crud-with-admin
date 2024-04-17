d5a4f61f95077d4970caf1c4499f2dbf99aa127d
WebDevCF2m <gitweb@cf2m.be>
2024-04-17 12:09:05 +0200

    homepage.view

--- public/index.php
+++ public/index.php
@@ -6,8 +6,28 @@ Front Controller
 
 */
 
+// lancement de la session
+session_start();
+
 // appel de dépendances
+// configuration
 require_once "../config.php";
+// Modèles
+require_once "../model/administratorModel.php";
+require_once "../model/geolocModel.php";
 
+// connexion à la DB 
+try{
+    // instanciation de notre connexion PDO
+    $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=". DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT , DB_LOGIN, DB_PASSWORD, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
 
+}catch(Exception $e){
+    die($e->getMessage());
+}
 
+// router
+
+// appel du contrôleur public
+require_once "../controller/publicController.php";
+
+$db = null;
