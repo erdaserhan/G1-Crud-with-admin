88a5e201f5f1a803f47128003c3c470caefbcdb5
WebDevCF2m <gitweb@cf2m.be>
2024-04-19 10:20:24 +0200

    style.css

--- controller/publicController.php
+++ controller/publicController.php
@@ -3,5 +3,7 @@
 Gère le site pour un visiteur non connecté
 */
 
+// si on est sur l'accueil chargement de tous les `geoloc`
+$datas = getAllGeoloc($db); // on obtient un string (Erreur SQL), un tableau vide (Pas de datas), un tableau non vide (On a des datas)
 // appel de la vue
 include "../view/public/homepage.view.html.php";
--- view/public/homepage.view.html.php
+++ view/public/homepage.view.html.php
@@ -4,8 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <?= __FILE__ ?>
+    <h1>Accueil</h1>
+    <nav>
+        <ul>
+            <li><a href="./">Accueil</a></li>
+            <li><a href="?connect">Connexion</a></li>
+        </ul>
+    </nav>
+    <div id="content">
+        <h3>Liste de nos lieux</h3>
+        <?php
+        // datas est une chaîne de caractère : erreur SQL !
+        if(is_string($datas)):
+        ?>
+            <h3 id="alert"><?=$datas?></h3>
+        <?php
+        // Pas encore de `geoloc`
+        elseif(empty($datas)):
+        ?>
+            <h3 id="comment">Pas encore de lieu.</h3>
+        <?php
+        // Nous avons des lieux
+        else:
+            // on compte le nombre de données 
+            $nb = count($datas);
+        ?>
+            <h3>Il y a <?=$nb?> <?=$nb>1 ? "lieux" : "lieu"?></h3>
+
+        <?php
+        endif;
+        ?>
+    </div>
 </body>
 </html>
